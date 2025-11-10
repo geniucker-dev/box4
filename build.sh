47cee31878bdb8bed3beded4595a26caa815e401
geniucker-dev <me@geniucker.com>
2025-11-11 07:21:07 +0800

    add yacd webui

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-zip -r -o -X -ll box4_$(cat module.prop | grep 'version=' | awk -F '=' '{print $2}').zip ./ -x '.git/*' -x 'build.sh' -x '.github/*' -x 'box4.json'
+git clone -b gh-pages https://github.com/MetaCubeX/Yacd-meta webroot
+
+zip -r -o -X -ll box4_$(cat module.prop | grep 'version=' | awk -F '=' '{print $2}').zip ./ -x '.git/*' -x 'webroot/.git/*' -x 'build.sh' -x '.github/*' -x 'box4.json'
+
+rm -r webroot
